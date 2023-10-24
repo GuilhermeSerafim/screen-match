e14f98f33e43d9ee071b9c787c3fe5347bf6c21d
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-24 13:12:10 -0300

    Entendendo para que serve os metodos setters e usando ele no projeto

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -3,20 +3,22 @@ package br.com.alura.screenmatch.modelos;
 //Classe é o modelo
 public class Filme {
     //O que um filme tem? - Variaveis
-    public String nome;
-    public int anoDeLancamento;
-    boolean incluidoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    public int duracaoEmMinutos;
+    private int duracaoEmMinutos;
+
+
     //O que um filme faz? - Métodos
     //Quando uma função está dentro de uma classe, nomeamos como metodo (comportamento daquela classe)
-
     //void - apenas faz, não retorna nada
     public void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome); //br.com.alura.screenmatch.modelos.Filme@e9e54c2
         System.out.println("Ano de lancaçamento: " + anoDeLancamento); //1970
     }
+
     public void avalia(double nota) {
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
@@ -31,4 +33,15 @@ public class Filme {
     public int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
     }
+
+    //O this é usado aqui para pegar o contexto da variavel local
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    //Para facilitar o entendimento
+//    public void setNome(String n) {
+//        nome = n;
+//    } nome = variavel local | n = parametro a ser passado e inserido na variavel local
+
+
 }
